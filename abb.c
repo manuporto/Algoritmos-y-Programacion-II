e413603b38f9631051a582143d40c02cbf03d024
Ignacio Sueiro <iasueiro@gmail.com>
2014-10-13 02:06:09 -0300

    En abb.h: 	-Se agrega la documentacion de abb_crear, abb_guardar, abb_obtener y 	abb_pertenece
    En abb.c:
    	-Se agrega la implementación de las funciones antes mencionadas
    	-Falta abb_borrar y abb_destruir

--- abb.c
+++ abb.c
@@ -11,7 +11,7 @@
  *  DEFINICION DE ESTRUCTURAS
  *-----------------------------------------------------------------------------
  */
- 
+
 struct abb{
 	nodo_abb_t *raiz;
 	size_t cantidad;
@@ -20,7 +20,111 @@ struct abb{
 };
 
 typedef nodo_abb{
+	char *clave;
+	void *dato;
 	struct nodo_abb_t *izq;
 	struct nodo_abb_t *der;
-} nodo_abb;
+} nodo_abb_t;
 
+/*-----------------------------------------------------------------------------
+ *  PRIMITIVAS DE NODO ABB
+ *-----------------------------------------------------------------------------*/
+
+// Crea un nodo_hash.
+// Post: crea un nodo_hash con una clave y un dato.
+static nodo_abb_t *nodo_crear(char *clave, void *dato){
+    nodo_abb_t *nodo_abb = malloc(sizeof(nodo_abb_t));
+    if(!nodo_hash) return NULL;
+    
+    char *clave_copia = malloc(sizeof(char)*strlen(clave)+1);
+    if(!clave_copia){
+    	free(nodo_abb);
+    	return NULL;
+    }
+    
+    strcpy(clave_copia, clave)
+    nodo_abb->clave = clave_copia;
+    nodo_abb->dato = dato;
+    nodo_abb->izq = NULL;
+    nodo_abb->der = NULL;
+
+    return nodo_hash;
+}
+
+// Destruye el par clave-dato.
+// Pre: el nodo fue creado.
+static void nodo_destruir(nodo_abb_t *nodo_abb, void destruir_dato(void*))
+{
+    free(nodo_hash->clave);
+    if(destruir_dato) destruir_dato(nodo_hash->dato);
+    free(nodo_hash);
+}
+
+/*-----------------------------------------------------------------------------
+ *  PRIMITIVAS DEL ÁRBOL
+ *-----------------------------------------------------------------------------*/
+
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
+	abb_t *abb = malloc(sizeof(abb_t));
+	if(!abb) return NULL;
+	abb->raiz = NULL;
+	abb->comparar = cmp;
+	abb->destruir_dato = destruir_dato;
+	abb->cantidad = 0;
+	return abb;
+}
+
+bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
+	nodo_abb_t *nodo_nuevo = nodo_crear(clave, dato);
+	if(!nodo_nuevo) return false;
+	//Caso A: el árbol estaba vacio
+	if(!arbol->raiz){
+		arbol->raiz = nodo_nuevo;
+		return true;
+	}
+	//Caso B: tengo que recorrer el árbol
+	nodo_abb_t *nodo_actual = arbol->raiz;
+	while(nodo_actual){
+		int cmp = arbol->comparar(clave, nodo_actual->clave);
+		// La clave es mas grande que la actual. Avanzo a la derecha
+		if(cmp > 0) nodo_actual = nodo_actual->der;
+		// La clave es mas chica que la actual. Avanzo a la izquierda
+		if(cmp < 0) nodo_actual = nodo_actual->izq;
+		if(cmp == 0){
+			//Significa que encontré un nodo con la misma clave. Hay que reempla
+			//zarlo
+			
+			//Que hago con el dato cuando lo tengo que reemplazar. Lo destruyo
+			// o dejo que el usuario se encargue?
+			nodo_actual->dato = nodo_nuevo->dato;
+			nodo_destruir(nodo_nuevo, NULL);
+			return true;
+		}
+	}
+	// La clave no existía y estoy parado en un NULL en la posición donde debo
+	// insertar el nuevo nodo
+	nodo_actual = nodo_nuevo;
+	arbol->cantidad += 1;
+	return true;
+}
+
+void *abb_obtener(const abb_t *arbol, const char *clave){
+	nodo_abb_t nodo_actual = arbol->raiz;
+	while(nodo_actual){
+		int cmp = arbol->comparar(clave, nodo_actual->clave);
+		if(cmp > 0) nodo_actual = nodo_actual->der;
+		if(cmp < 0) nodo_actual = nodo_actual->izq;
+		if(cmp == 0) return nodo_actual->dato;
+	}
+	// Si llegó hasta acá, significa que la clave no está presente
+	return NULL;
+}
+
+bool abb_pertenece(const abb_t *arbol, const char *clave){
+	if(!abb_obtener(arbol, clave)) return false;
+	return true;
+}
+
+size_t abb_cantidad(abb_t *arbol){
+	return arbol->cantidad;
+}
