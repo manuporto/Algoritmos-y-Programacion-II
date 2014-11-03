780f8bc924486c7cc2ce6b48cf96bac036ede5dd
Manuel Ignacio <mporto@fi.uba.ar>
2014-11-03 11:43:48 -0300

    Se agrega archivo tp2.c que representa el flujo del programa (a grandes rasgos).

---
+++ tp2.c
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tp2.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/11/14 11:21:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "lectura.h"
+#include "sistema.h"
+
+int main(){
+    const char twittear[] = "twittear";
+    const char favorito[] = "favorito";
+    const char buscar[] = "buscar";
+
+    char *linea = leer_linea();
+    sistema_t *sistema = sistema_crear();
+    while(!feof(stdin)){
+        char *comando = strtok(NULL, " ");
+        if(!strcmp(twittear, comando)){
+            char *nombre = strtok(linea, " ");
+            char *twit = strtok(NULL, "");
+            sistema_twittear(sistema, nombre, twit);
+        }else if(!strcmp(favorito, comando)){
+            char *id = strtok(NULL, "");
+            sistema_favorito(sistema, nombre, id);
+        }else if(!strcmp(buscar, comando){
+            char *buscado = strtok(NULL, " ");
+            char *orden = strtok(NULL, " ");
+            char *cantidad = strtok(NULL, "");
+            sistema_buscar(sistema, buscado, orden, cantidad);
+        }
+        free(linea);
+        linea = leer_linea();
+    }
+
+    free(linea);
+    sistema_destruir(sistema);
+    return 0;
+
+}
